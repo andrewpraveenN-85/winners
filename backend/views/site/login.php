cdc23c3823dac4fc4a3d566844fa8b3037ecd689
andrewpraveenN-85 <andrew.praveen.n.mobile@gmail.com>
2025-03-09 14:56:55 +0530

    fixes

--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -28,7 +28,7 @@ $this->title = "Login";
             </div>
             <?= Html::submitButton('Log in', ['class' => 'btn btn-warning w-100', 'name' => 'login-button']) ?>
             <p class="mt-3 text-center"><?= Html::a('Forgot Password?', ['site/request-password-reset'], ['class' => 'text-warning text-decoration-none']) ?></p>
-            <p class="mt-3 text-center">Not a member? <?= Html::a('Join as a member!', ['site/register'], ['class' => 'text-warning text-decoration-none']) ?></p>
+            <p class="mt-3 text-center"><?= Html::a('Join as a member!', ['site/register'], ['class' => 'text-warning text-decoration-none']) ?></p>
             <?php ActiveForm::end(); ?>
         </div>
 
