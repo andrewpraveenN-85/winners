5f8dbc0a814659649e79ca58f85c1dff40fd2596
andrewpraveenN-85 <andrew.praveen.n.mobile@gmail.com>
2025-03-10 15:25:53 +0530

    fixes

--- common/config/params.php
+++ common/config/params.php
@@ -7,7 +7,7 @@ return [
     'user.passwordResetTokenExpire' => 3600,
     'user.passwordMinLength' => 8,
     'uploadPathIMG' => dirname(dirname(__DIR__)) . '/backend/web/uploads/',
-    'back_host' => 'http://winners.local/uploads/',
+    'back_host' => (YII_ENV_DEV ? 'http://winners.local/uploads/' : 'http://winners.jaan.lk/uploads/'),
     'config_constants' => [
         'PACKAGE_UPGRADE' => 'PACKAGE_UPGRADE'
     ],
